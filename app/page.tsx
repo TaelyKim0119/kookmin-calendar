d9d6637f1562c241da9e48ee357762c5857f900f
TaelyKim0119 <taely00@naver.com>
2025-03-02 17:35:19 +0900

    Add Next.js configuration files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>국민대학교 학사일정</h1>
+    </main>
+  );
+} 
